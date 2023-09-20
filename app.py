5e2a2a45e4099fa3818f02b9d3fda657191168de
platanob <bcarrasco2022@alu.uct.cl>
2023-09-20 20:35:44 -0300

    arreglo_conexion_bd

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask, request
-from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://benja:123@bananashop.tzmfwsy.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://benja:123@bananashop.tzmfwsy.mongodb.net/?retryWrites=true&w=majority'
-mongo = PyMongo(app)
 
 @app.route('/registro', methods=['POST'])
 def create_user():
-    nombre = request.json.get('nombre')
-    telefono = request.json.get('telefono')
-    rut = request.json.get('rut')
-    direccion = request.json.get('direccion')
-    contra = request.json.get('contra')
+    nombre = request.json.get("nombre")
+    telefono = request.json.get("telefono")
+    rut = request.json.get("rut")
+    direccion = request.json.get("direccion")
+    contra = request.json.get("contra")
 
     if nombre and telefono and rut and direccion and contra:
         contra_hashed = generate_password_hash(contra)
-        id = mongo.db.users.insert(
+        id = client.bananashop.users.insert_one(
             {'nombre': nombre, 'telefono': telefono, 'rut': rut, 'direccion': direccion, 'contra': contra_hashed}
         )
         response = {
