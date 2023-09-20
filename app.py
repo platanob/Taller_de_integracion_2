ad420840dfc89cbf4e7f5345ffcedb907b92663e
platanob <bcarrasco2022@alu.uct.cl>
2023-09-20 20:46:30 -0300

    agregacion de comprobaciones

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://benja:123@bananashop.tzmfwsy.mongodb.net/?retryWrites=true&w=majority"
-
-# Create a new client and connect to the server
+uri = "mongodb+srv://benja:123@bananashop.tzmfwsy.mongodb.net/?retryWites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
-
-
 app = Flask(__name__)
+
+def conbd():
+    if client is None:
+        return {'message': 'Error al conectar a la base de datos'}, 500
 
 @app.route('/registro', methods=['POST'])
 def create_user():
@@ -18,6 +18,8 @@ def create_user():
     rut = request.json.get("rut")
     direccion = request.json.get("direccion")
     contra = request.json.get("contra")
+
+    conbd()
 
     if nombre and telefono and rut and direccion and contra:
         contra_hashed = generate_password_hash(contra)
