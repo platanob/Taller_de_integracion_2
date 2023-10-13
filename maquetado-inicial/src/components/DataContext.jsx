c137cd7dbe883a2cdb1e402ede23fae489398708
pabloantivil <pabloantivil16@gmail.com>
2023-10-12 23:40:15 -0300

    Mostrar productos (BackEnd)
    Mostrar productos que están en la base de datos

--- maquetado-inicial/src/components/DataContext.jsx
+++ maquetado-inicial/src/components/DataContext.jsx
@@ -1,15 +1,21 @@
-import { createContext , useState, useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 export const dataContext = createContext();
 
 const DataProvider = ({ children }) => {
-    const [data, setData] = useState([]);
+// Estado data que almacenará los datos de los productos
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios("data.json").then((res) => setData(res.data));
-    },[]);
-    return <dataContext.Provider value={{ data }}>{children}</dataContext.Provider>;
+  useEffect(() => {
+    // Realiza una peticion GET a la URL de la API de productos en el servidor Flask.
+    axios.get("http://127.0.0.1:5000/productos").then((res) => 
+    // Actualiza el estado con los datos de la respuesta
+    setData(res.data));
+  }, []);
+
+  return <dataContext.Provider value={{ data }}>{children}</dataContext.Provider>;
 };
 
+// Exporta el componente DataProvider
 export default DataProvider;
--- maquetado-inicial/src/components/products.jsx
+++ maquetado-inicial/src/components/products.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { useContext } from 'react';
-import { dataContext } from './DataContext';
+import React, { useContext, useEffect } from "react";
+import { dataContext } from "./DataContext";
+import axios from "axios";
 
- 
 const Products = () => {
-  const {data} = useContext(dataContext);
-  return data.map((product)=>{
-    return (
-      <div className="card">
-        <h3>{product.name}</h3>
-        <h3>{product.sexo}</h3>
-        <h4>{product.precio}</h4>
-        <button>Comprar</button>
-      </div>
-    );
-  });
+  // Obtiene los datos de productos del contexto.
+  const { data } = useContext(dataContext);
+
+  useEffect(() => {
+    // Peticion GET al servidor Flask para obtener productos.
+    axios.get("http://127.0.0.1:5000/productos")
+      .then((res) => {
+      })
+      .catch((error) => {
+        console.error("Error al recuperar los productos: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {data.map((product) => (
+        <div className="card" key={product._id}> {/* Producto debe tener una clave unica */}
+          <h3>{product.nombre}</h3>
+          <h3>{product.genero}</h3>
+          <h4>{product.costo}</h4>
+          <button>Comprar</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Products;
